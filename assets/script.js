818e4bce7c5225e20ebff7b2f5762c4d74936363
Kevin Cleary <kjc2274@yahoo.com>
2021-12-13 09:26:09 -0500

    update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,19 @@
+
+
 async function getMovie(){
     const url = `https://imdb-api.com/en/API/SearchMovie/k_qfgt5lxg/leon the professional`;
     const res = await axios.get(url);
-    const movie = res.data;
+    const movie = res.data.results[0].id;
     console.log(movie);
+    return movie;
+    
 };
-getMovie();
+// getMovie();
+
+async function getCast(){
+    const url = `https://imdb-api.com/en/API/FullCast/k_qfgt5lxg/tt0110413`;
+    const res = await axios.get(url);
+    const cast = res.data;
+    console.log(cast);
+};
+// getCast();
