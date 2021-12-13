c31895c8c80b0e228ad17fc89591a7d2713a290b
Kevin Cleary <kjc2274@yahoo.com>
2021-12-13 16:59:46 -0500

    finished intitial movie search and display funtionality. Accidently did my previous commits to a lesson I pulled up for reference :(

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,62 @@
+const search1 = document.querySelector("#input1");
+const search2 = document.querySelector("#input2");
+const searchInput1 = document.querySelector("#search1");
+const searchInput2 = document.querySelector("#search2");
+const button = document.querySelector("#button1");
+const movieDisplay1 = document.querySelector("#display1");
+const movieDisplay2 = document.querySelector("#display2");
 
-
-async function getMovie(title){
-    const url = `https://imdb-api.com/en/API/SearchMovie/k_qfgt5lxg/${title}`;
+async function getMovie1(event){
+    event.preventDefault();
+    const title = searchInput1.value;
+    const url = `http://www.omdbapi.com/?apikey=390956fa&s=${title}`;
     const res = await axios.get(url);
-    const movie = res.data.results[0].id;
-    console.log(movie);
-    return movie;
-    
+    const movie = res.data.Search[0];
+    displayMovie1(movie);
+    const id = res.data.Search[0].imdbID;
+    console.log(id);
+    // return movie;    
 };
-// getMovie();
+
+async function getMovie2(event){
+    event.preventDefault();
+    const title = searchInput2.value;
+    const url = `http://www.omdbapi.com/?apikey=390956fa&s=${title}`;
+    const res = await axios.get(url);
+    const movie = res.data.Search[0];
+    displayMovie2(movie);
+    const id = res.data.Search[0].imdbID;
+    console.log(id);
+    // return movie;    
+};
+
+function displayMovie1(movie){
+    movieDisplay1.innerHTML = "";
+    document.getElementById("display1").style.padding = "0";
+
+    const title = document.createElement("h3");
+    title.innerText = movie.Title;
+    movieDisplay1.appendChild(title);
+
+    const poster = document.createElement("img");
+    poster.src = movie.Poster;
+    poster.alt = `${movie.Title} poster`;
+    movieDisplay1.appendChild(poster);
+};
+
+function displayMovie2(movie){
+    movieDisplay2.innerHTML = "";
+    document.getElementById("display2").style.padding = "0";
+
+    const title = document.createElement("h3");
+    title.innerText = movie.Title;
+    movieDisplay2.appendChild(title);
+
+    const poster = document.createElement("img");
+    poster.src = movie.Poster;
+    poster.alt = `${movie.Title} poster`;
+    movieDisplay2.appendChild(poster);
+};
 
 async function getCast(movie){
     const url = `https://imdb-api.com/en/API/FullCast/k_qfgt5lxg/${movie}`;
@@ -16,4 +64,15 @@ async function getCast(movie){
     const cast = res.data;
     console.log(cast);
 };
-// getCast();
+
+async function getCast(movie){
+    const url = `https://imdb-api.com/en/API/FullCast/k_qfgt5lxg/${movie}`;
+    const res = await axios.get(url);
+    const cast = res.data;
+    console.log(cast);
+};
+
+
+
+search1.addEventListener("submit", getMovie1);
+search2.addEventListener("submit", getMovie2);
