05d8ea14cb201e3acf5c6cd47070650d5fde2c92
Kevin Cleary <kjc2274@yahoo.com>
2021-12-14 20:36:52 -0500

    some CSS work

--- assets/script.js
+++ assets/script.js
@@ -80,14 +80,14 @@ async function getCast2(movie){
     const url = `https://imdb-api.com/en/API/FullCast/k_qfgt5lxg/${movie}`;
     const res = await axios.get(url);
     const cast = res.data.actors;
-    console.log(cast);
+    // console.log(cast);
     castArray2 = [];
     actorImages = [];
     for (let i = 0; i < cast.length; i++){
     castArray2.push(cast[i].name);
     actorImages.push(cast[i].image)
     }
-    console.log(actorImages);
+    // console.log(actorImages);
 };
 
 function findActors(event){
@@ -96,16 +96,23 @@ function findActors(event){
     document.getElementById("actor-display").style.padding = "0 0 100px 0";
     let newArray = castArray1.concat(castArray2);
 
-    //next 8 lines taken from: https://stackoverflow.com/questions/840781/get-all-non-unique-values-i-e-duplicate-more-than-one-occurrence-in-an-array
+    
+    //next 7 lines taken from: https://stackoverflow.com/questions/840781/get-all-non-unique-values-i-e-duplicate-more-than-one-occurrence-in-an-array
     const count = names =>
   names.reduce((a, b) => ({ ...a,
     [b]: (a[b] || 0) + 1
   }), {}) 
-
-const duplicates = dict =>
-  Object.keys(dict).filter((a) => dict[a] > 1)
+    const duplicates = dict =>
+    Object.keys(dict).filter((a) => dict[a] > 1)
     let actors = duplicates(count(newArray));
+    
+    
     // console.log(actors);
+    if(actors.length == 0){
+        let h3 = document.createElement('h3');
+        h3.innerText = "Sorry, it looks like we don't have anyone who was in both films you selected.";
+        actorDisplay.appendChild(h3);
+    }else{
   for(let i = 0; i < actors.length; i++) {
     let actorName = document.createElement('h4');
     actorName.innerText = `${actors[i]}`;
@@ -114,10 +121,11 @@ const duplicates = dict =>
     let actorIndex = castArray2.indexOf(actors[i]);
     let actorImage = document.createElement('img');
     actorImage.src = actorImages[actorIndex];
-    actorImage.alt = `Image of ${actorName}`;
+    actorImage.alt = `Image of ${actors[i]}`;
     actorName.appendChild(actorImage);
     actorImage.style.width = '5vw' 
   };
+}
 };
 
 
